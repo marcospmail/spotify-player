c041f5a5d252556f97c786cd2b200e10ef29a506
Marcos Paulo <marcosp.mail@gmail.com>
2020-11-19 14:47:04 -0300

    chore: App rename

--- ios/playerTests/playerTests.m
+++ ios/playerTests/playerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface sevenappstestTests : XCTestCase
+@interface playerTests : XCTestCase
 
 @end
 
-@implementation sevenappstestTests
+@implementation playerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
